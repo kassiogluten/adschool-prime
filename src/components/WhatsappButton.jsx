c579d05fa451a13720fc47e22e9d8894ec6c54f2
Kassio Linux <kassiogluten@gmail.com>
2022-02-11 19:09:25 -0300

    whatsapp number update

--- src/components/WhatsappButton.jsx
+++ src/components/WhatsappButton.jsx
@@ -10,7 +10,7 @@ export const WhatsappButton = () => (
     bottom={4}
     right={1}
     as="a"
-    href="https://api.whatsapp.com/send/?phone=5533991957171&text=Ol%C3%A1%21+Gostaria+de+tirar+algumas+d%C3%BAvidas%21&app_absent=0"
+    href="https://api.whatsapp.com/send/?phone=553391033595&text=Ol%C3%A1%21+Gostaria+de+tirar+algumas+d%C3%BAvidas%21&app_absent=0"
     target="_blank"
   >
     <Image src="/whatsapp.png" alt="Chamar no Whatsapp" layout="fill" />
